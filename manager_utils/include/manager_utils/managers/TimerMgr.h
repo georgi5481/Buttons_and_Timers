34276931221470905857511472b6da9ff1476add
George <georgi5481@abv.bg>
2022-10-22 22:12:03 +0300

    adding timer manager into the implementation as another singleton

---
+++ manager_utils/include/manager_utils/managers/TimerMgr.h
@@ -0,0 +1,21 @@
+#ifndef MANAGER_UTILS_INCLUDE_MANAGER_UTILS_MANAGERS_TIMERMGR_H_
+#define MANAGER_UTILS_INCLUDE_MANAGER_UTILS_MANAGERS_TIMERMGR_H_
+
+//C system includes
+
+//C++ system includes
+
+//3rd-party includes
+
+//Own includes
+
+//Forward Declaration
+
+class TimerMgr{
+	TimerMgr();
+	~TimerMgr();
+
+
+};
+
+#endif /* MANAGER_UTILS_INCLUDE_MANAGER_UTILS_MANAGERS_TIMERMGR_H_ */
